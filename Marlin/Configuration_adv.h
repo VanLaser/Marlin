dc2aac55879d1dc8acc85926d06390aef837fcba
Van Laser <gabi_laser@yahoo.com>
2019-05-18 02:03:06 +0300

    stealthchop

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1551,7 +1551,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT     596  // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT     525  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
   #endif
@@ -1563,7 +1563,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT     596
+    #define Y_CURRENT     525
     #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
   #endif
@@ -1575,7 +1575,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT     596
+    #define Z_CURRENT     550
     #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
   #endif
@@ -1593,7 +1593,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT    709
+    #define E0_CURRENT    600
     #define E0_MICROSTEPS  16
     #define E0_RSENSE    0.11
   #endif
@@ -1716,7 +1716,7 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  #define HYBRID_THRESHOLD
+  //#define HYBRID_THRESHOLD
 
   #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
